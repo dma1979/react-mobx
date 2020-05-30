9ceb88bed90b5623de81e7270a3492229533565e
Dzmitry Marudau <dmarudau@cpaglobal.com>
2020-05-30 19:04:19 +0300

    Basic React app with mobx

---
+++ src/js/TodoList.js
@@ -0,0 +1,59 @@
+import React from "react"
+import {observer} from "mobx-react"
+
+@observer
+export default class TodoList extends React.Component {
+    render() {
+        const {todos} = this.props.store
+        const todoList = todos.map(todo => (
+            <li>{todo}</li>
+        ))
+
+        return (
+            <div>
+                <h1>TODOS</h1>
+                <ul>{todoList}</ul>
+            </div>
+        )
+    }
+
+    /*
+    createNew(e) {
+        if (e.which === 13) {
+            this.props.store.createTodo(e.target.value)
+            e.target.value = ""
+        }
+    }
+
+    filter(e) {
+        this.props.store.filter = e.target.value
+    }
+
+    toggleComplete(todo) {
+        todo.complete = !todo.complete
+    }
+
+    render() {
+
+     const {clearComplete, filter, filteredTodos, todos} = this.props.store
+
+        const todoLis = filteredTodos.map(todo => (
+            <li key={todo.id}>
+                <input type="checkbox"
+                       onChange={this.toggleComplete.bind(this, todo)}
+                       value={todo.complete}
+                       checked={todo.complete}/>
+                <span>{todo.value}</span>
+            </li>
+        ))
+        return <div>
+            <h1>todos</h1>
+            <input className="new" onKeyPress={this.createNew.bind(this)}/>
+            <input className="filter" value={filter} onChange={this.filter.bind(this)}/>
+            <ul>{todoLis}</ul>
+            <a href="#" onClick={clearComplete}>Clear Complete</a>
+        </div>
+    }
+        */
+
+}
---
+++ src/js/TodoStore.js
@@ -0,0 +1,41 @@
+import { computed, observable, autorun } from "mobx"
+
+class Todo {
+  @observable value
+  @observable id
+  @observable complete
+
+  constructor(value) {
+    this.value = value
+    this.id = Date.now()
+    this.complete = false
+  }
+}
+
+export class TodoStore {
+  @observable todos = ["read article", "watch video"]
+  @observable filter = ""
+
+  @computed get filteredTodos() {
+    var matchesFilter = new RegExp(this.filter, "i")
+    return this.todos.filter(todo => !this.filter || matchesFilter.test(todo.value))
+  }
+
+/*  createTodo(value) {
+    this.todos.push(new Todo(value))
+  }
+
+  clearComplete = () => {
+    const incompleteTodos = this.todos.filter(todo => !todo.complete)
+    this.todos.replace(incompleteTodos)
+  }*/
+}
+var store = window.store =  new TodoStore()
+export default store
+//export default new TodoStore
+
+autorun(() => {
+  console.log(store.filter)
+  console.log(store.todos[0])
+})
+
